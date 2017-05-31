cedc5928ba679bc3810f74d775977f2f5ef4f5cf
dandanloveJM <gigi834921748@qq.com>
2017-06-01 00:48:47 +0800

    删除localstorage

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -5,39 +5,38 @@ import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import 'normalize.css'
 import './reset.css'
-import * as localStore from './localStore'
 import AV from './leanCloud'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       newTodo: 'test',
-      todoList: localStore.load('todoList') || []
+      todoList: []
     }
-    
+
   }
   render() {
     let Todo = this
     let todos = this.state.todoList
-    .filter(function(item){
-      return !item.deleted
-    })
-    .map(function(item, index){
-      return (
-        <li key={index}>
-          <TodoItem todo={item} onToggle={Todo.toggle.bind(Todo)} 
-          onDelete={Todo.delete.bind(Todo)}/>
-        </li>
-      )
-    })
+      .filter(function (item) {
+        return !item.deleted
+      })
+      .map(function (item, index) {
+        return (
+          <li key={index}>
+            <TodoItem todo={item} onToggle={Todo.toggle.bind(Todo)}
+              onDelete={Todo.delete.bind(Todo)} />
+          </li>
+        )
+      })
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} 
-          onSubmit={this.addTodo.bind(this)}
-          onChange={this.changeTitle.bind(this)} />
+          <TodoInput content={this.state.newTodo}
+            onSubmit={this.addTodo.bind(this)}
+            onChange={this.changeTitle.bind(this)} />
         </div>
         <ol className="todoList">
           {todos}
@@ -45,21 +44,20 @@ class App extends Component {
       </div>
     );
   }
-  componentDidUpdate(){
-    localStore.save('todoList', this.state.todoList)
+  componentDidUpdate() {
   }
-  delete(event,todo){
+  delete(event, todo) {
     todo.deleted = true
     this.setState(this.state)
     console.log(todo.deleted)
-    
+
   }
-  toggle(e,todo){
-    todo.status = todo.status === 'completed'? '' : 'completed'
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed'
     this.setState(this.state)
     //localStore.save('todoList', this.state.todoList)
   }
-  addTodo(event){
+  addTodo(event) {
     this.state.todoList.push({
       id: idMaker(),
       title: event.target.value,
@@ -68,27 +66,27 @@ class App extends Component {
     })
 
     this.setState({
-      newTodo:'',
+      newTodo: '',
       todoList: this.state.todoList
     })
     //localStore.save('todoList', this.state.todoList)
-    
+
   }
-  changeTitle(event){
+  changeTitle(event) {
     this.setState({
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
     //localStore.save('todoList', this.state.todoList)
   }
-  
+
 }
 
 export default App;
 
 let id = 0
 
-function idMaker(){
+function idMaker() {
   id += 1
   return id
 }
