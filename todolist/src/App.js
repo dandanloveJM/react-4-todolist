388dcca9de30e253ebbf5cabe1d0bc007150036c
dandanloveJM <gigi834921748@qq.com>
2017-06-01 00:00:47 +0800

    初始化Leancloud

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -6,6 +6,11 @@ import TodoItem from './TodoItem'
 import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
+import AV from 'leancloud-storage'
+
+const appId = 'c5RYbFTv2A2PAEG7baR6fEY2-gzGzoHsz';
+const appKey = 'j7WqqztI5UkEt6NIC91erTDv';
+AV.init({ appId, appKey });
 
 class App extends Component {
   constructor(props){
